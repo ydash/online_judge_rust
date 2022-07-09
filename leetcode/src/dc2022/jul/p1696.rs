3a974f409992ae9511cc41b83f702869ac218613
ydash <52343777+ydash@users.noreply.github.com>
2022-07-09 17:28:25 +0900

    optimize time complexity: O(n*logk) -> O(n)

--- leetcode/src/dc2022/jul/p1696.rs
+++ leetcode/src/dc2022/jul/p1696.rs
@@ -29,45 +29,25 @@ mod tests {
 
 // 1696. Jump Game VI
 mod solution {
-    use std::collections::BinaryHeap;
-
-    #[derive(Debug, PartialEq, Eq, PartialOrd)]
-    struct State {
-        cost: i32,
-        pos: usize,
-    }
-
-    impl Ord for State {
-        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-            self.cost.cmp(&other.cost)
-        }
-    }
+    use std::collections::VecDeque;
 
     #[allow(dead_code)]
     pub fn max_result(nums: Vec<i32>, k: i32) -> i32 {
         let k = k as usize;
-        let mut dp = BinaryHeap::new();
-        dp.push(State {
-            cost: nums[0],
-            pos: 0,
-        });
-        for current in 1..nums.len() {
-            if current > k {
-                while dp.peek().map(|s| s.pos < current - k).unwrap_or(false) {
-                    dp.pop();
-                }
+        let mut dp = vec![0; nums.len()];
+        let mut deq = VecDeque::new();
+        dp[0] = nums[0];
+        deq.push_back(0);
+        for i in 1..nums.len() {
+            if i > k && deq.front().map(|&j| j < i - k).unwrap_or(false) {
+                deq.pop_front();
             }
-            dp.peek()
-                .map(|s| State {
-                    cost: s.cost + nums[current],
-                    pos: current,
-                })
-                .map(|s| dp.push(s));
+            dp[i] = dp[*deq.front().unwrap()] + nums[i];
+            while !deq.is_empty() && dp[*deq.back().unwrap()] <= dp[i] {
+                deq.pop_back();
+            }
+            deq.push_back(i);
         }
-
-        while dp.peek().map(|s| s.pos != nums.len() - 1).unwrap_or(false) {
-            dp.pop();
-        }
-        dp.pop().unwrap().cost
+        dp[nums.len() - 1]
     }
 }
