e5da8a8c0ec1de25a9c43fd3c5ddded2b8ea8b3c
ydash <52343777+ydash@users.noreply.github.com>
2022-07-25 21:18:44 +0900

    make code mroe concise

--- leetcode/src/dc2022/jul/p34.rs
+++ leetcode/src/dc2022/jul/p34.rs
@@ -1,7 +1,5 @@
 // 34. Find First and Last Position of Element in Sorted Array
 
-use std::cmp::Ordering;
-
 struct Solution {}
 impl Solution {
     #[allow(dead_code)]
@@ -10,51 +8,40 @@ impl Solution {
         if nums.is_empty() {
             return result;
         }
-        let start = Self::binary_search(&nums, target, |v, mid, _left, right| {
-            if mid == 0 || v[mid - 1] != target {
-                Some(mid as i32)
-            } else {
-                *right = mid;
-                None
-            }
-        });
-        if start != -1 {
-            let end = Self::binary_search(&nums, target, |v, mid, left, _right| {
-                if mid == v.len() - 1 || v[mid + 1] != target {
-                    Some(mid as i32)
-                } else {
-                    *left = mid + 1;
-                    None
-                }
-            });
-            result[0] = start;
-            result[1] = end;
+
+        let mut left = 0;
+        let mut right = nums.len();
+        left = Self::binary_search(&nums, left, right, |v, mid| v[mid] < target);
+        if left >= nums.len() || nums[left] != target {
+            return result;
         }
+        result[0] = left as i32;
+
+        right = nums.len();
+        right = Self::binary_search(&nums, left, right, |v, mid| v[mid] <= target);
+        result[1] = right as i32 - 1;
 
         result
     }
 
-    fn binary_search<F: Fn(&Vec<i32>, usize, &mut usize, &mut usize) -> Option<i32>>(
+    fn binary_search<F: Fn(&Vec<i32>, usize) -> bool>(
         nums: &Vec<i32>,
-        target: i32,
-        f: F,
-    ) -> i32 {
-        let mut size = nums.len();
-        let mut left = 0;
-        let mut right = size;
+        left: usize,
+        right: usize,
+        pred: F,
+    ) -> usize {
+        let mut left = left;
+        let mut right = right;
         while left < right {
+            let size = right - left;
             let mid = left + size / 2;
-            match nums[mid].cmp(&target) {
-                Ordering::Less => left = mid + 1,
-                Ordering::Equal => match f(&nums, mid, &mut left, &mut right) {
-                    Some(result) => return result,
-                    None => (),
-                },
-                Ordering::Greater => right = mid,
+            if pred(&nums, mid) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
-            size = right - left;
         }
-        -1
+        left
     }
 }
 
@@ -95,6 +82,12 @@ mod tests {
     #[test]
     fn test_case_06() {
         let result = Solution::search_range(vec![1], 0);
+        assert_eq!(vec![-1, -1], result)
+    }
+
+    #[test]
+    fn test_case_07() {
+        let result = Solution::search_range(vec![2, 2], 3);
         assert_eq!(vec![-1, -1], result)
     }
 }
