68a361f0237a04e0614e510b7a580083e5c6a601
ydash <52343777+ydash@users.noreply.github.com>
2022-07-26 19:43:21 +0900

    refactor: replace qualified path

--- leetcode/src/dc2022/jul/p240.rs
+++ leetcode/src/dc2022/jul/p240.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 // 240. Search a 2D Matrix II
 struct Solution {}
 impl Solution {
@@ -8,9 +10,9 @@ impl Solution {
 
         while col < m && row < n {
             match matrix[col][n - row - 1].cmp(&target) {
-                std::cmp::Ordering::Less => col += 1,
-                std::cmp::Ordering::Equal => return true,
-                std::cmp::Ordering::Greater => row += 1,
+                Ordering::Less => col += 1,
+                Ordering::Equal => return true,
+                Ordering::Greater => row += 1,
             }
         }
 
