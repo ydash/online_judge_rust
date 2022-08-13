f881015efdb73a7eaf92f1ce853fe850c0d22483
ydash <52343777+ydash@users.noreply.github.com>
2022-08-14 06:16:25 +0900

    improve time complexity

--- leetcode/src/dc2022/aug/p30.rs
+++ leetcode/src/dc2022/aug/p30.rs
@@ -6,35 +6,40 @@ struct Solution {}
 impl Solution {
     #[allow(dead_code)]
     pub fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
-        fn check(s: &str, word_len: usize, word_num: usize, count: &HashMap<&str, i32>) -> bool {
-            let mut seen = HashMap::new();
-            for j in 0..word_num {
-                let sub_str = &s[word_len * j..word_len * (j + 1)];
-                let c2 = seen.entry(sub_str).or_insert(0);
-                if *c2 < *count.get(sub_str).unwrap_or(&0) {
-                    *c2 += 1;
-                } else {
-                    return false;
-                }
-            }
-            true
-        }
         let word_len = words[0].len();
-        let m = words.len();
         let n = s.len();
-        if n < m * word_len {
-            return vec![];
-        }
+        let substr_len = words.len() * word_len;
         let mut result = vec![];
         let mut count: HashMap<&str, i32> = HashMap::new();
         for word in words.iter() {
             let entry = count.entry(word).or_insert(0);
             *entry += 1;
         }
-        for i in 0..=(n - word_len * m) {
-            if check(&s[i..], word_len, m, &count) {
-                result.push(i as i32);
+        let mut seen: HashMap<&str, i32> = HashMap::new();
+        for i in 0..word_len {
+            let mut left = i;
+            let mut right = left;
+            while right + word_len <= n {
+                let word = &s[right..right + word_len];
+                if count.contains_key(word) {
+                    if seen.get(word).unwrap_or(&0) < &count[&word] {
+                        *seen.entry(word).or_insert(0) += 1;
+                        right += word_len;
+                        if right - left == substr_len {
+                            result.push(left as i32);
+                        }
+                    } else {
+                        let word = &s[left..left + word_len];
+                        *seen.entry(word).or_insert(0) -= 1;
+                        left += word_len;
+                    }
+                } else {
+                    seen.clear();
+                    left = right + word_len;
+                    right = left;
+                }
             }
+            seen.clear();
         }
         result
     }
@@ -89,5 +94,14 @@ mod tests {
             util::str_vec_2_string_vec(vec!["a", "a"]),
         );
         assert_eq!(Vec::<i32>::new(), result)
+    }
+
+    #[test]
+    fn test_case_06() {
+        let result = Solution::find_substring(
+            String::from("aaaaaa"),
+            util::str_vec_2_string_vec(vec!["aa", "aa"]),
+        );
+        assert_eq!(vec![0, 2, 1], result)
     }
 }
