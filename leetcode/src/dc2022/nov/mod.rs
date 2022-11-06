0a8ef596a855db84a1aac6d8c791781945029a33
ydash <52343777+ydash@users.noreply.github.com>
2022-11-06 20:00:04 +0900

    add solution of Orderly Queue

--- leetcode/src/dc2022/nov/mod.rs
+++ leetcode/src/dc2022/nov/mod.rs
@@ -3,3 +3,4 @@ mod p212;
 mod p2131;
 mod p345;
 mod p433;
+mod p899;
---
+++ leetcode/src/dc2022/nov/p899.rs
@@ -0,0 +1,55 @@
+// 899. Orderly Queue
+
+struct Solution {}
+impl Solution {
+    #[allow(dead_code)]
+    pub fn orderly_queue(mut s: String, k: i32) -> String {
+        if k < 2 {
+            let bytes = s
+                .as_bytes()
+                .clone()
+                .to_owned()
+                .into_iter()
+                .collect::<Vec<_>>();
+            let min = *bytes.iter().min().unwrap();
+            (0..s.len())
+                .filter(|&i| bytes[i] == min)
+                .map(|i| {
+                    let mut rotated = bytes[i..].to_vec();
+                    rotated.extend_from_slice(&bytes[..i]);
+                    let s = String::from_utf8(rotated).unwrap();
+                    s
+                })
+                .min()
+                .unwrap()
+        } else {
+            unsafe {
+                s.as_bytes_mut().sort();
+            }
+            s
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_case_01() {
+        let result = Solution::orderly_queue("cba".to_owned(), 1);
+        assert_eq!("acb", result)
+    }
+
+    #[test]
+    fn test_case_02() {
+        let result = Solution::orderly_queue("baaca".to_owned(), 3);
+        assert_eq!("aaabc", result)
+    }
+
+    #[test]
+    fn test_case_03() {
+        let result = Solution::orderly_queue("dacaba".to_owned(), 1);
+        assert_eq!("abadac", result)
+    }
+}
