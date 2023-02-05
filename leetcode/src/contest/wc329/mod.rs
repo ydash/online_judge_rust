53ea99b012e030cee23b0b99f3d5cda524c5583c
ydash <52343777+ydash@users.noreply.github.com>
2023-02-05 22:47:04 +0900

    remove ignored file

--- leetcode/src/contest/wc329/mod.rs
+++
@@ -1,4 +0,0 @@
-mod p6296;
-mod p6297;
-mod p6298;
-mod p6299;
--- leetcode/src/contest/wc329/p6298.rs
+++
@@ -1,9 +0,0 @@
-//  6298. Apply Bitwise Operations to Make Strings Equal
-
-struct Solution {}
-impl Solution {
-    #[allow(dead_code)]
-    pub fn make_strings_equal(s: String, target: String) -> bool {
-        s.as_bytes().iter().max() == target.as_bytes().iter().max()
-    }
-}
